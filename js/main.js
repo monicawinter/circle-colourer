a55918b66271b66cc3504a47a50750bdb15b0641
monicawinter <m.m.winter@hotmail.com>
2016-03-17 12:46:52 -0400

    changed size of ball and added class

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 var $html = $('html');
-var $ball = $('div');
+var $ball = $('.ball');
 var $pick = $('form');
 var $input = $('#pick');
 
-
-$pick.on('change', function() {
+$pick.on('change', function () {
   console.log('works');
   $ball.css('background-color', $input.val());
-  });
+});
